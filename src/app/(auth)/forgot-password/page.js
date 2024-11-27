d7a3a89b603b8503d798e773dc6e30191bf44ddf
Rakotomalala Fahasoavana Johann Luciano <jolu032302@gmail.com>
2024-11-27 10:10:35 +0300

    Ajout des pages Forgot Password, Login, Register et une page de test

---
+++ src/app/(auth)/forgot-password/page.js
@@ -0,0 +1,9 @@
+const Forgot = () => {
+  return (
+    <>
+      <h1>Forgot Password</h1>
+    </>
+  )
+}
+
+export default Forgot
---
+++ src/app/_test/page.js
@@ -0,0 +1,9 @@
+const test = () => {
+  return (
+    <>
+      <h1>This page could not be render</h1>
+    </>
+  )
+}
+
+export default test
