4d8a9a90268f716f5ae65ede833667e0a235ed42
Rakotomalala Fahasoavana Johann Luciano <jolu032302@gmail.com>
2024-11-30 04:55:07 +0300

    handle PATCH request

--- src/app/welcome/[id]/route.js
+++ src/app/welcome/[id]/route.js
@@ -4,3 +4,11 @@ export const GET = async (_request, { params }) => {
   const com = comment.find((comme) => comme.id === parseInt(params.id))
   return Response.json(com)
 }
+
+export const PATCH = async (request, { params }) => {
+  const body = await request.json()
+  const { text } = body
+  const index = comment.findIndex((com) => com.id === parseInt(params.id))
+  comment[index].text = text
+  return Response.json(comment[index])
+}
