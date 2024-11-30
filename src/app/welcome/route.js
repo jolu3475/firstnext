132a98ab6eb1be27556d6d3e7ac3c34f91892c37
Rakotomalala Fahasoavana Johann Luciano <jolu032302@gmail.com>
2024-11-30 04:36:40 +0300

    handle post request

--- src/app/welcome/route.js
+++ src/app/welcome/route.js
@@ -1,5 +1,20 @@
 import { comment } from './data'
 
-export const GET = () => {
+export const GET = async () => {
   return new Response.json(comment)
+}
+
+export const POST = async (request) => {
+  const com = await request.json()
+  const newComment = {
+    id: comment.length + 1,
+    content: com.text,
+  }
+  comment.push(newComment)
+  return new Response(JSON.stringify(newComment), {
+    headers: {
+      'content-type': 'application/json',
+    },
+    status: 201,
+  })
 }
